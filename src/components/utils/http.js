bc78ecc6060d9fd39af6eeb741177a4cc638e15e
ShamiulIslam388 <shamiulislam388@gmail.com>
2023-11-09 13:39:53 +0600

    initial commit with setup react-query and fetch data with query

---
+++ src/components/utils/http.js
@@ -0,0 +1,17 @@
+import { QueryClient } from "@tanstack/react-query";
+export const queryClient = new QueryClient();
+
+export const fetchEvents = async () => {
+  const response = await fetch("http://localhost:3000/events");
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+};
